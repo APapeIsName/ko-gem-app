8aedfd37dec302abc17ad4add8e59a81a131d4b2
APapeIsName <gms72901217@gmail.com>
2025-08-02 18:05:46 +0900

    [Chore] 주석 추가

--- components/auth/login/AuthPlatformButton.tsx
+++ components/auth/login/AuthPlatformButton.tsx
@@ -1,5 +1,11 @@
 import { Pressable, StyleSheet } from 'react-native';
 
+/**
+ * OAuth 플랫폼 버튼 컴포넌트
+ * @param children - 버튼 내부에 표시할 컴포넌트
+ * @param onPress - 버튼 클릭 시 호출될 함수
+ * @returns 
+ */
 export default function AuthPlatformButton({
     children,
     onPress,
